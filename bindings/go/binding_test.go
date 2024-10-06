070ddf1a436aa9e65b8245dff0045de637ac143a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-10-06 07:34:44 +0200

    Update tree-sitter-cli and regenerate bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_haml_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-haml"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_haml "github.com/tree-sitter/tree-sitter-haml/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
