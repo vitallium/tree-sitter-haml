aad8d7bb1732403fbe18452f122bce52f8f0520b
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-11-11 07:09:28 +0100

    Fix Swift bindings

--- Package.swift
+++ Package.swift
@@ -4,44 +4,34 @@ import PackageDescription
 let package = Package(
     name: "TreeSitterHaml",
     products: [
-        .library(name: "TreeSitterHaml", targets: ["TreeSitterHaml"]),
+        .library(name: "TreeSitterHaml", targets: ["TreeSitterHaml"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0")
+    ],
     targets: [
-        .target(name: "TreeSitterHaml",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    // NOTE: if your language has an external scanner, add it here.
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterHaml",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterRubyTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterHaml",
+            ],
+            path: "bindings/swift/TreeSitterHamlTests"
+        ),
     ],
     cLanguageStandard: .c11
 )
